b10e69d7c0e9342ba8a4e37488e6a95c645c7541
Davi.reis <reisdavi@hotmail.com>
2023-10-27 18:09:58 -0300

    Aula de Polimorfismo e Herança

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
 ﻿using Aula_Abstracao_Encapsulamento.Models;
 
+
+Professor p1 = new Professor();
+p1.Nome = "Eduardo";
+p1.Idade = 40;
+p1.Salario = 1000;
+p1.Apresentar();
+
+/////////////////////////////////////
+
+Aluno a1 = new Aluno();
+
+a1.Nome = "Davi";
+a1.Idade = 36;
+a1.Nota = 10;
+
+a1.Apresentar();
+
+/////////////////////////////////////////////////////
+
 ContaCorrente c1 = new ContaCorrente(123, 1000);
 
 c1.ExibirSaldo();
